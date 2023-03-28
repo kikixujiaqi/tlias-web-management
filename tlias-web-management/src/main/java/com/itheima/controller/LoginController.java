36ed5fd4b85820232036110a51f3d1467d36b431
沈浔 <1464979993@qq.com>
2023-03-28 15:14:01 +0800

    JWT令牌功能完成

--- tlias-web-management/src/main/java/com/itheima/controller/LoginController.java
+++ tlias-web-management/src/main/java/com/itheima/controller/LoginController.java
@@ -3,11 +3,15 @@ package com.itheima.controller;
 import com.itheima.pojo.Emp;
 import com.itheima.pojo.Result;
 import com.itheima.service.EmpService;
+import com.itheima.utils.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -20,6 +24,14 @@ public class LoginController {
     public Result login(@RequestBody Emp emp) {
         log.info("员工登录:{}", emp);
         Emp e = empService.login(emp);
-        return e != null ? Result.success() : Result.error("用户名或密码错误");
+        if (e != null) {
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("id", e.getId());
+            claims.put("name", e.getName());
+            claims.put("username", e.getUsername());
+            String jwt = JwtUtils.generateJwt(claims);
+            return Result.success(jwt);
+        }
+        return Result.error("用户名或密码错误");
     }
 }
--- tlias-web-management/src/test/java/com/itheima/TliasWebManagementApplicationTests.java
+++ tlias-web-management/src/test/java/com/itheima/TliasWebManagementApplicationTests.java
@@ -1,7 +1,15 @@
 package com.itheima;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @SpringBootTest
 class TliasWebManagementApplicationTests {
@@ -10,4 +18,34 @@ class TliasWebManagementApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    public void testUuid() {
+        for (int i = 0; i < 1000; i++) {
+            String uuid = UUID.randomUUID().toString();
+            System.out.println(uuid);
+        }
+    }
+
+    @Test
+    public void testGenJwt() {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", 1);
+        claims.put("name", "tom");
+
+        String jwt = Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, "itheima")
+                .setClaims(claims)
+                .setExpiration(new Date(System.currentTimeMillis() + 3600 * 1000))
+                .compact();
+        System.out.println(jwt);
+    }
+
+    @Test
+    public void testParseJwt() {
+        Claims claims = Jwts.parser()
+                .setSigningKey("itheiam")
+                .parseClaimsJws("eyJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoidG9tIiwiaWQiOjEsImV4cCI6MTY3OTk4Nzk4Nn0.lz449HuTP654STFn7h3ArRW_e5-E8AjO3p_rrY_vf1o")
+                .getBody();
+        System.out.println(claims);
+    }
 }
