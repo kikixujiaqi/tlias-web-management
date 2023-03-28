b1623214301db5941c4022e50adfba7f5b962162
沈浔 <1464979993@qq.com>
2023-03-28 16:21:42 +0800

    完成拦截器interceptor功能

---
+++ tlias-web-management/src/main/java/com/itheima/interceptor/LoginInterceptor.java
@@ -0,0 +1,56 @@
+package com.itheima.interceptor;
+
+import com.alibaba.fastjson.JSONObject;
+import com.itheima.pojo.Result;
+import com.itheima.utils.JwtUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String url = request.getRequestURI().toString();
+        log.info("请求的url:{}", url);
+        if (url.contains("login")) {
+            return true;
+        }
+        String jwt = request.getHeader("token");
+        if (! StringUtils.hasLength(jwt)) {
+            log.info("请求头token为空，返回未登陆信息");
+            Result error = Result.error("NOT_LOGIN");
+            String notLogin = JSONObject.toJSONString(error);
+            response.getWriter().write(notLogin);
+            return false;
+        }
+        try {
+            JwtUtils.parseJWT(jwt);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("解析令牌失败，返回未登陆信息");
+            Result error = Result.error("NOT_LOGIN");
+            String notLogin = JSONObject.toJSONString(error);
+            response.getWriter().write(notLogin);
+            return false;
+        }
+        log.info("令牌合法，放行");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandler运行了");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("afterCompletion运行了");
+    }
+}
